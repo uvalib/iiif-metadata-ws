604e8c77bd51cdec5ed7b6f5adbddf6b0754788c
Dave Goldstein <dpg3k@virginia.edu>
2020-09-02 12:15:07 -0400

    Bring in packaging assets. Make a layered build. Update to latest Alpine

---
+++ package/scripts/entry.sh
@@ -0,0 +1,8 @@
+# run application
+
+# run from here, since application expects json template in templates/
+cd bin; ./iiifsrv -tracksys $TRACKSYS_URL -apollo $APOLLO_URL -solr $IIIF_MAN_SOLR_URL -host $IIIF_MANIFEST_HOST -iiif $IIIF_URL
+
+#
+# end of file
+#
