5f2ad9a23f62f2260204674e3038eaa0d6eedc31
Dave Goldstein <dpg3k@virginia.edu>
2020-10-28 10:12:45 -0400

    Support S3 configuration attributes

--- iiifsrv/config.go
+++ iiifsrv/config.go
@@ -7,12 +7,14 @@ import (
 
 // configuration data
 type serviceConfig struct {
-	port        int
-	hostName    string
-	solrURL     string
-	tracksysURL string
-	apolloURL   string
-	iiifURL     string
+	port         int
+	hostName     string
+	solrURL      string
+	tracksysURL  string
+	apolloURL    string
+	iiifURL      string
+	cacheBucket  string
+	cacheRootUrl string
 }
 
 func loadConfig() *serviceConfig {
@@ -24,14 +26,18 @@ func loadConfig() *serviceConfig {
 	flag.StringVar(&cfg.solrURL, "solr", "http://solr.lib.virginia.edu:8082/solr/core", "Virgo Solr URL")
 	flag.StringVar(&cfg.hostName, "host", "iiifman.lib.virginia.edu", "Hostname for this service")
 	flag.StringVar(&cfg.iiifURL, "iiif", "https://iiif.lib.virginia.edu", "IIIF image server")
+	flag.StringVar(&cfg.cacheBucket, "bucket", "virgo4-iiif-manifest-staging", "cache bucket name")
+	flag.StringVar(&cfg.cacheRootUrl, "rooturl", "https://s3.us-east-1.amazonaws.com", "cache root URL")
 	flag.Parse()
 
-	log.Printf("[CONFIG] port        = [%d]", cfg.port)
-	log.Printf("[CONFIG] tracksysURL = [%s]", cfg.tracksysURL)
-	log.Printf("[CONFIG] apolloURL   = [%s]", cfg.apolloURL)
-	log.Printf("[CONFIG] solrURL     = [%s]", cfg.solrURL)
-	log.Printf("[CONFIG] hostName    = [%s]", cfg.hostName)
-	log.Printf("[CONFIG] iiifURL     = [%s]", cfg.iiifURL)
+	log.Printf("[CONFIG] port         = [%d]", cfg.port)
+	log.Printf("[CONFIG] tracksysURL  = [%s]", cfg.tracksysURL)
+	log.Printf("[CONFIG] apolloURL    = [%s]", cfg.apolloURL)
+	log.Printf("[CONFIG] solrURL      = [%s]", cfg.solrURL)
+	log.Printf("[CONFIG] hostName     = [%s]", cfg.hostName)
+	log.Printf("[CONFIG] iiifURL      = [%s]", cfg.iiifURL)
+	log.Printf("[CONFIG] cacheBucket  = [%s]", cfg.cacheBucket)
+	log.Printf("[CONFIG] cacheRootUrl = [%s]", cfg.cacheRootUrl)
 
 	return &cfg
 }
