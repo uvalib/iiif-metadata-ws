65d18491a8c1a16c3d3b55664ffd440ff4a79176
Dave Goldstein <dpg3k@virginia.edu>
2020-11-03 07:44:46 -0500

    Correct tracksys URL for Solr queries

--- iiifsrv/xml_parser.go
+++ iiifsrv/xml_parser.go
@@ -79,7 +79,7 @@ import (
 // parseTracksysSolr will get the solr add record from TrackSys and parse it for metdata elements
 func parseTracksysSolr(tracksysURL string, data *IIIF) error {
 	// For XML metadata
-	url := fmt.Sprintf("%s/solr/%s?no_external=1", tracksysURL, data.MetadataPID)
+	url := fmt.Sprintf("%s/api/solr/%s?no_external=1", tracksysURL, data.MetadataPID)
 	log.Printf("INFO: get Solr record: %s", url)
 	resp, err := getAPIResponse(url, standardHttpClient)
 	if err != nil {
